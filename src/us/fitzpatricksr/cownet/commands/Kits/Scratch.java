61910870e58541cbf56716c7a9ec167b69a350fb
jfitzpatrick <minecraft@fitzpatricksr.us>
2013-01-08 08:45:33 -0800

    Adding some notes for upcoming kits.

---
+++ src/us/fitzpatricksr/cownet/commands/Kits/Scratch.java
@@ -0,0 +1,40 @@
+package us.fitzpatricksr.cownet.commands.Kits;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jfitzpatrick
+ * Date: 1/8/13
+ * Time: 8:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Scratch {
+
+
+    /*
+
+    for each class
+        for each level
+           inventory
+           armor colors
+           enchantments
+
+
+
+
+    */
+
+    public String[] getKits() {
+        return null;
+    }
+
+    public int getMaxLevel(String kitName) {
+        return 1;
+    }
+
+    public void awardKit(Player player, String kitName, int kitLevel) {
+
+    }
+
+}
