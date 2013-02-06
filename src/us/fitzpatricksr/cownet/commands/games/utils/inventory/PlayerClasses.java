b5da3487152b44653e59480a9f3091a4cefd487b
jfitzpatrick <minecraft@fitzpatricksr.us>
2013-02-06 08:54:10 -0800

    Support for loading class definitions from a config file.  Renamed PlayerClass to PlayerClasses

--- src/us/fitzpatricksr/cownet/commands/games/utils/inventory/PlayerClasses.java
+++ src/us/fitzpatricksr/cownet/commands/games/utils/inventory/PlayerClasses.java
@@ -1,22 +1,37 @@
 package us.fitzpatricksr.cownet.commands.games.utils.inventory;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import us.fitzpatricksr.cownet.utils.PersistentState;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 
 /**
  * A predefined set of items that can be given to a player.
  */
-public class PlayerClass {
-    private static HashMap<String, ClassKit> classes;  // kit name -> kit
+public class PlayerClasses {
+    private static HashMap<String, ClassKit> classes;  // class name -> class
     private static HashMap<String, ClassKit> playerClasses; // playerName -> class
 
-    public static void setClass(Player player, String className) {
+    public void defineClass(String name, ItemStack... items) {
+        classes.put(name, new ClassKit(name, items));
+    }
+
+    public Set<String> getClassNames() {
+        return classes.keySet();
+    }
+
+    public ItemStack[] getClassContents(String name) {
+        return classes.get(name).kitItems;
+    }
+
+    public void setClass(Player player, String className) {
         String playerName = player.getName();
         ClassKit oldPlayerClass = playerClasses.get(playerName);
         if (oldPlayerClass != null) {
@@ -30,35 +45,56 @@ public class PlayerClass {
         }
     }
 
-    public static String getClass(Player player) {
+    public String getClass(Player player) {
         ClassKit playerClass = playerClasses.get(player.getName());
         return (playerClass != null) ? playerClass.getName() : "";
     }
 
     /* Clear the players inventory and make it match the contents of the kit */
-    public static void installKit(Player player) {
+    public void installKit(Player player) {
         getKit(player).installKit(player);
     }
 
     /* Top up the players current inventory by adding kit items as needed so that
      * they have at least as much of each item as a new kit. */
-    public static void refreshKit(Player player) {
+    public void refreshKit(Player player) {
         getKit(player).refreshKit(player);
     }
 
     /* Remove all items of types that may have come from the kit. */
-    public static void removeKit(Player player) {
+    public void removeKit(Player player) {
         getKit(player).removeKit(player);
     }
 
     /* Remove anything not in the kit from inventory */
-    public static void enforceKit(Player player) {
+    public void enforceKit(Player player) {
         getKit(player).enforceKit(player);
     }
 
-    private static ClassKit getKit(Player player) {
+    private ClassKit getKit(Player player) {
         ClassKit kit = playerClasses.get(player.getName());
         return (kit != null) ? kit : new ClassKit();
+    }
+
+    /*
+   Resource format is:
+   rootNode:
+     className:
+       material: count
+       material: count
+    */
+    public void loadClasses(String rootNode, PersistentState storage) {
+        ConfigurationSection classes = storage.getConfigurationSection(rootNode);
+        for (String className : classes.getKeys(false)) {
+            LinkedList<ItemStack> stacks = new LinkedList<ItemStack>();
+            ConfigurationSection materials = classes.getConfigurationSection(className);
+            for (String materialName : materials.getKeys(false)) {
+                int count = materials.getInt(materialName);
+                Material material = Material.getMaterial(materialName);
+                stacks.add(new ItemStack(material, count));
+            }
+            defineClass(className, stacks.toArray(new ItemStack[stacks.size()]));
+        }
     }
 
     private static class ClassKit {
@@ -131,6 +167,4 @@ public class PlayerClass {
             return materialInKit.contains(stuff);
         }
     }
-
-    //hey jf - so we want to add some serialize/deserialize stuff here?
 }
